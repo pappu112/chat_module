8a13db17d0903193f3d1d4d7ae3881795d09b7d8
iFixit <ifixit@iFixits-MacBook-Air-3.local>
2020-07-13 23:08:27 +0600

    first commit

---
+++ app/src/main/java/com/pappu/chatapp/Fragments/APIService.java
@@ -0,0 +1,21 @@
+package com.pappu.chatapp.Fragments;
+
+import com.pappu.chatapp.Notifications.MyResponse;
+import com.pappu.chatapp.Notifications.Sender;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @Headers(
+            {
+                    "Content-Type:application/json",
+                    "Authorization:key=ADD HERE YOUR KEY FROM FIREBASE PROJECT"
+            }
+    )
+
+    @POST("fcm/send")
+    Call<MyResponse> sendNotification(@Body Sender body);
+}
---
+++ app/src/main/java/com/pappu/chatapp/Notifications/MyResponse.java
@@ -0,0 +1,6 @@
+package com.pappu.chatapp.Notifications;
+
+public class MyResponse {
+
+    public int success;
+}
